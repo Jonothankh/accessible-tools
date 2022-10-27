833ff156e50989e361b4afcea654ae094ca5ec4a
Jonothan Hunt <jonothankh@gmail.com>
2022-10-27 17:55:13 +0100

    Update footer and meta theme colour

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ const Footer = (props) =>
 {
     return (
         <footer>
-            <p className='copyright-notice'>This site is by <a href='https://jonothan.dev'>Jonothan</a>. If you wish to contribute, use <a href='https://forms.gle/QuubaSXYPf2cChSE9'>this form</a>.</p>
+            <p className='notice'>This site is by <a href='https://jonothan.dev'>Jonothan</a>. </p>
+            <p className='notice'>If you wish to contribute, use <a href='https://forms.gle/QuubaSXYPf2cChSE9'>this form</a>.</p>
         </footer>
     )
 
